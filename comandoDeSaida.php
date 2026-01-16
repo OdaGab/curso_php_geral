c9820a352a36a4277fb96e6c2f45dd2503c171c9
Odair <odavita32@gmail.com>
2026-01-15 22:36:21 -0300

    Comandos de saída

---
+++ comandoDeSaida.php
@@ -0,0 +1,22 @@
+<?php 
+   // comandos de saída
+    
+   // Exibe uma mensagem na tela
+    echo "Olá, mundo!"; 
+
+   // Outra forma de exibir uma mensagem na tela
+    print "Bem-vindo ao PHP."; 
+
+    // Formata e exibe um número 
+    printf("Número formatado: %.2f", 123.456);   
+
+    // Exibe múltiplos valores
+    echo 'a', 'b', 'c'; 
+
+    // Exibe uma mensagem usando parênteses
+    print ('Dentro de parênteses');
+
+    // Exibe informações detalhadas sobre uma variável
+    var_dump("Teste de var_dump");
+
+   ?>
