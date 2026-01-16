c3984841e258bc3bdac77f3b664a8431a30b600b
Odair <odavita32@gmail.com>
2026-01-16 14:09:35 -0300

    Tipo Númerico

---
+++ numerico.php
@@ -0,0 +1,29 @@
+<?php
+    // codigo PHP
+
+    // Variável do tipo Númerico
+    // Em PHP, vamos falar do Tipo Númerico que pode ser dividido em dois subtipos principais: Integer (inteiro) e Float (ponto flutuante ou número decimal)    
+    // Variável do tipo Integer
+    // O tipo Integer representa números inteiros, ou seja, números sem parte decimal. Eles podem ser positivos ou negativos.
+    // Exemplos de declaração de variáveis do tipo Integer
+
+    $idade = 25; // Número inteiro positivo
+    $saldoBancario = -1500; // Número inteiro negativo
+
+    // Exibindo o valor das variáveis do tipo Integer
+
+    echo "Idade: " . $idade . "\n";
+    echo "Saldo Bancário: " . $saldoBancario . "\n";
+
+    // Variável do tipo Float
+    // O tipo Float representa números com parte decimal. Eles também podem ser positivos ou negativos.
+    // Exemplos de declaração de variáveis do tipo Float
+
+    $altura = 1.75; // Número decimal positivo
+    $temperatura = -3.5; // Número decimal negativo
+
+    // Exibindo o valor das variáveis do tipo Float
+    
+    echo "Altura: " . $altura . "m\n";
+    echo "Temperatura: " . $temperatura . "°C\n";
+?>
