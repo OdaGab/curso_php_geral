0428a2bae9251eb4d8fe7c5f85448a1cab2cf393
Odair <odavita32@gmail.com>
2026-01-17 16:31:12 -0300

    Variáveis globais

---
+++ variaveisGlobais.php
@@ -0,0 +1,29 @@
+<?php
+    // Exemplo de variáveis globais em PHP
+    $variavelGlobal = "Esta é uma variável global";
+
+    function mostrarVariavelGlobal() {
+        global $variavelGlobal;
+        echo $variavelGlobal;
+    }
+
+    mostrarVariavelGlobal();
+?>
+<?php
+    // Exercícios
+    // Teste em outros senários 
+    // Crie outras variáveis globais e acesse-as dentro de funções diferentes
+    
+
+
+
+    $outraVariavelGlobal = "Outra variável global";     
+    function mostrarOutraVariavelGlobal() {
+        global $outraVariavelGlobal;
+        echo $outraVariavelGlobal;
+    }
+
+    
+
+    mostrarOutraVariavelGlobal();
+?>
